5b512c68147376ad6e41ec2bc8d616a12b01cebb
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2024-02-08 18:11:50 +0100

    cc

--- ej1.py
+++ ej1.py
@@ -1,15 +1,56 @@
-
 class Tarjeta_de_credito:
-    def __init__(self, cliente, balance, limite, banco, cuenta):
+    def __init__(self, cliente, limite, banco, cuenta, balance=0 ):
         self._cliente = cliente
-        self._balance= balance
         self._limite= limite
         self._banco = banco
         self._cuenta = cuenta
+        self._balance= balance
         
-    def descripcion(self):
-        return "El cliente {self._cliente} en el banco {self._banco}, una cuenta {self._cuenta} con un balance de {self._balance} y un limite de {self._limite} "
+    def __str__(self):
+        return f"El cliente {self._cliente} en el banco {self._banco}, una cuenta {self._cuenta} con un balance de {self._balance} y un limite de {self._limite} "
+        
+    def get_cliente(self):
+        return self._cliente
     
-a1= Tarjeta_de_credito("Olivia", 10, 2000, "Santander", 2374382409352)
+    def get_balance(self):
+        return self._balance
+    
+    def get_banco(self):
+        return self._banco
+    
+    def get_limite(self):
+        return self._limite  
+    
+    def get_cuenta(self):
+        return self._cuenta
+    
+    
+a1= Tarjeta_de_credito("Olivia", 2000, "Santander", 2374382409352, 10)
+print(a1)
 
-a1.descripcion()
+cliente = a1.get_cliente()
+print("Cliente:", cliente)
+
+banco = a1.get_banco()
+print("Banco:", banco)
+
+balance= a1.get_balance()
+print("Balance:", balance)
+
+limite= a1.get_limite()
+print("Limite:", limite)
+
+cuenta= a1.get_cuenta()
+print("Cuenta:", cuenta)
+
+cargo= int(input("Cargar un precio a la tarjeta de credito:"))
+
+balance1= balance + cargo
+
+if balance1 < limite:
+    print(f"Se ha cargado con exito, ahora el balance es de {balance1}.") 
+else: 
+    print("No se ha podido realizar el cargo")
+
+#cargo(precio)?
+#realizar_pago(cantidad)?
