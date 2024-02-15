77894c797bb360803977204b63a22af80bf9aed0
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2024-02-15 09:02:56 +0100

    cc

--- entrega1/e1.py
+++ entrega1/e1.py
@@ -6,25 +6,31 @@ for _ in range(10):
     ecosistema= random.choice(["osos", "peces", "none"])
     rio.append(ecosistema)
     
+print(f"Primera lista: {rio}")
 
 
-def mover_animales(rio):
+def mover_none(rio):
     for i in range(len(rio)):
         if rio[i] in [ "osos", "peces" ]:
             mover= random.choice([True, False])
             if mover:
                 if i < len(rio) - 1 and rio[i+1]== "none":
                     rio[i], rio[i+1]= rio[i+1], rio[i]
+
+
+def mover_rio(rio):
+    i=0
+    for i in range(len(rio)):
+        if rio[i] in [ "osos", "peces" ]:
+                if i < len(rio)-1 and rio[i+1]== "none":
+                    rio[i], rio[i+1]= rio[i+1], rio[i]
+                i= i+2
                     
-                if i < len(rio) - 1 and rio[i] == rio[i + 1]:
-                    rio.append("none")
+
                 
-                    
-                    
-                    
-print(f"Primera lista: {rio}")
 
-mover_animales(rio)
 
-print(f"Segunda lista: {rio}")
-
+#def mover_colision(rio):
+    
+mover_rio(rio)
+print(f"Tercera lista:{rio}")
